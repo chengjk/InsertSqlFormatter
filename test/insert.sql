c901eae5dd2a756be678e6e1962f673baff1b0d8
jacky <jacky.cheng@yeahmobi.com>
2019-07-27 16:43:14 +0800

    add test

---
+++ test/insert.sql
@@ -0,0 +1,10 @@
+insert into user ("name","age","gender") values ("中文测试",20,"male");
+
+
+insert into user ("name","age","gender") 
+	values ("中文测试",20,"male");
+
+insert into user
+ ("name","age","gender")
+  values 
+  ("中文测试",20,"male");
---
+++ test/insert_without_name.sql
@@ -0,0 +1,4 @@
+insert into `user` values ("中文测试",20,"male");
+
+insert into `user` values 
+	("中文测试",20,"male");
